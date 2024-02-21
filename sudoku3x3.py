f7ad582d402e6547368f0ae834ef9e505b5537c1
Andres16HKMA <kamzyx2004@gmail.com>
2024-02-21 07:03:50 -0500

    arreglo

--- sudoku3x3.py
+++ sudoku3x3.py
@@ -1,27 +1,13 @@
 from flask import Flask, jsonify, request
-import json
-
-app = Flask(__name__)
 
 
 class Cuadros():
-    def _init_(self):
-        self.cuadrito = 0
-        self.number = 0
-        self.fila = 0
-        self.columnas = 0
 
     def validador(self, sudoku):
-        # Reiniciar variables de instancia
-        self.cuadrito = 0
-        self.fila = 0
 
         for All_Sudoku in sudoku:
             fila = All_Sudoku["fila"]
-            self.fila += 1
             for cuadro in All_Sudoku["columnas"]:
-                self.cuadrito += 1
-
                 numeros_vistos = set()
                 for fila_cuadro in cuadro:
                     for numero in fila_cuadro:
